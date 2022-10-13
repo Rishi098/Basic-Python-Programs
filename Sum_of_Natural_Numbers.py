70fc8d81688f99ffd2ed71a508d1dc991f2ee84b
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-13 18:22:24 +0530

    Update Sum_of_Natural_Numbers.py

--- Sum_of_Natural_Numbers.py
+++ Sum_of_Natural_Numbers.py
@@ -1,14 +1,13 @@
+num=int(input("Enter a Natural Number:-"))
+sum = 0
 if num < 0:
    print("Enter a positive number")
 else:
-   sum = 0
    # use while loop to iterate until zero
    while(num > 0):
        sum += num
        num -= 1
    print("The sum is", sum)
    
-   
-   
-   OUTPUT:-
-   The sum is 136
+   for input num=52
+   Output:-1378
